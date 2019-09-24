9a291db0d5470441e466e63c62951c364e67f071
dontehightower <dontedhightower@gmail.com>
2019-09-24 16:21:34 -0500

    add links to header

--- src/components/header.js
+++ src/components/header.js
@@ -12,6 +12,9 @@ const Header = ({ siteTitle }) => (
     <div
       style={{
         margin: `0 auto`,
+        display: `flex`,
+        justifyContent: `space-between`,
+        alignItems: `center`,
         maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
       }}
@@ -27,6 +30,37 @@ const Header = ({ siteTitle }) => (
           {siteTitle}
         </Link>
       </h1>
+      <div
+        style={{
+          display: `grid`,
+          gridTemplateColumns: `1fr 1fr`,
+          gridGap: `8px`,
+          alignContent: `center`,
+        }}
+      >
+        <h4 style={{ margin: 0 }}>
+          <Link
+            to="/blog"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+            Blog
+          </Link>
+        </h4>
+        <h4 style={{ margin: 0 }}>
+          <Link
+            to="/notes"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+            Notes
+          </Link>
+        </h4>
+      </div>
     </div>
   </header>
 )
