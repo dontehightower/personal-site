e00930ffe92be97f1366b93d6225c0499cbd7bfc
dontehightower <dontedhightower@gmail.com>
2019-09-24 06:02:23 -0500

    add notes and blog theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,5 +30,19 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-theme-notes`,
+      options: {
+        // basePath defaults to `/`
+        basePath: `/notes`,
+      },
+    },
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {
+        // basePath defaults to `/`
+        basePath: `/blog`,
+      },
+    },
   ],
 }
